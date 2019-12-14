b3e13bc279329f80b2468d3701c93a910a5c6d8d
hq <hq_cml@163.com>
2019-12-14 12:36:13 +0800

    body.txt

--- post.lua
+++ post.lua
@@ -1,6 +1,6 @@
 idArr = {}
-falg = 1 
 function init(args)
+   falg = 1 
    for line in io.lines("body.txt") do
        print(line)
        idArr[falg] = line
@@ -12,7 +12,7 @@ end
 request = function()
    parms = idArr[falg%(table.getn(idArr) + 1)]
    --print(falg % table.getn(idArr))
-   --print(parms)
+   print(falg % table.getn(idArr), parms)
    falg = falg + 1
    return wrk.format("POST", "http://127.0.0.1:9527/post", nil, parms) 
 end
